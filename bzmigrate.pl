6683a05244cfccc016d607522047cd2c9cab2ffd
Rowan James <rowanj@phere.net>
2011-09-15 20:26:13 +1000

    Initial commit.

---
+++ bzmigrate.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use Term::ReadKey;
+use XML::Simple;
+use Data::Dumper;
+
+my $xml = new XML::Simple;
+my $root_xml = $xml->XMLin("bugzilla.xml");
+#print Dumper($root_xml);
+
+my @bugs = @{$root_xml->{'bug'}};
+#print Dumper(@bugs);
+
+foreach my $bug (@bugs)
+{
+#    print Dumper($bug);
+    print "\n-----------------------------------------------\n";
+    print "Bug: $bug->{'bug_id'}\n";
+    print "\t$bug->{'short_desc'}\n";
+    print "\t$bug->{'bug_status'}\n\n";
+  
+    foreach my $desc (@{$bug->{'long_desc'}} )
+    {
+	print "\t\t$desc->{'who'}{'name'}:\n";
+	print "\t$desc->{'thetext'}\n";
+    }
+#    die "dead.";
+}
+
+die "ending."
+
+print("login: ");
+#my $login = ReadLine(0);
+
+print("password: ");
+ReadMode('noecho');
+#my $password = ReadLine(0);
+print("\n");
+ReadMode('normal');
+
